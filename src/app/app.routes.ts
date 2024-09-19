27cafa407538c09937bc51fdf76c2dec9eff2477
Leonel Alegre <78889336+LeonelAlegreDev@users.noreply.github.com>
2024-09-19 22:26:50 +0000

    Se creo la pagina about-me Se añadio un acceso a about-me desde el home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginViewComponent } from './views/login-view/login-view.component';
 import { HomeViewComponent } from './views/home-view/home-view.component';
 import { SignupViewComponent } from './views/signup-view/signup-view.component';
+import { AboutMeComponent } from './views/about-me/about-me.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: "full" },
     { path: 'home', component: HomeViewComponent},
     { path: 'login', component: LoginViewComponent},
-    { path: 'signup', component: SignupViewComponent}
+    { path: 'signup', component: SignupViewComponent},
+    { path: 'about-me', component: AboutMeComponent}
 ];
