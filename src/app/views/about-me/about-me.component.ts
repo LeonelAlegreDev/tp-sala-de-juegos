8b703c7006cffd7f480ff4c410ba2e5b47ed3fa6
Leonel Alegre <78889336+LeonelAlegreDev@users.noreply.github.com>
2024-09-20 00:25:09 +0000

    Se añadieron correcciones para hacer

--- src/app/views/about-me/about-me.component.ts
+++ src/app/views/about-me/about-me.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../../components/menu/menu.component';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
