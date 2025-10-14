2ac775a933efe68a33e9db55ca0ff3de3c411712
Leonel ALegre <leonelalegre.dev@gmail.com>
2025-10-14 19:57:36 -0300

    Se modificaron las pantallas del juego ahorcado para mostrar el puntaje

--- src/app/components/juegos/ahorcado/ahorcado.component.ts
+++ src/app/components/juegos/ahorcado/ahorcado.component.ts
@@ -15,7 +15,7 @@ export class AhorcadoComponent {
   @ViewChildren('character') characterElements!: QueryList<ElementRef>;
   @ViewChildren('part') partElements!: QueryList<ElementRef>;
 
-  gameStateList = ['start', 'playing', 'win', 'lose'];
+  gameStateList = ['start', 'playing', 'end', 'score', 'dificulty'];
   gameState = this.gameStateList[0];
   teclado = [
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
@@ -27,63 +27,64 @@ export class AhorcadoComponent {
     vidas: 6,
     letras: [] as string[],
     partesPersonaje: {} as any,
-    letrasUsadas: [] as string[]
+    letrasUsadas: [] as string[],
+    result: '',
   };
   palabrasFacil = ['GATO', 'CASA', 'PAIS', 'CIELO', 'AGUA', 'PERRO', 'MESA', 'LUNA', 'BARCO', 'CAMION'];
   palabrasMedio = ['CARACOL', 'LIBRO', 'PLANTA', 'ESCUELA', 'PELOTA', 'MUSICA', 'FUTBOL', 'LLUVIA', 'TORMENTA', 'GALAXIA'];
   palabrasDificil = ['UNIVERSO', 'MURCIELAGO', 'HORIZONTE', 'COMPUTADORA', 'EXTRAORDINARIO', 'HELICOPTERO', 'ELECTRONICA', 'CIENTIFICO'];
 
-  Start(){
+  Start() {
     console.log("Juego iniciado");
-    this.gameState = this.gameStateList[1]; // playing
+    this.gameState = this.gameStateList[4]; // dificulty
     this.InitCharacter();
   }
-  Adivinar(letra: string){
-    if(this.run.vidas > 0){
+  Adivinar(letra: string) {
+    if (this.run.vidas > 0) {
       let adivinada = false;
-      let esGanador = true; 
+      let esGanador = true;
 
 
       // Valida si la letra ya fue usada
-      for(let j = 0; j < this.run.letrasUsadas.length; j++){
-        if(this.run.letrasUsadas[j] === letra){
+      for (let j = 0; j < this.run.letrasUsadas.length; j++) {
+        if (this.run.letrasUsadas[j] === letra) {
           console.log("Letra desactivada");
           return;
         }
       }
       this.run.letrasUsadas.push(letra);
-      
+
       // Comprueba si se ha adivinado la letra
-      for(let i = 0; i < this.run.palabra.length - 1; i++){
+      for (let i = 0; i < this.run.palabra.length - 1; i++) {
         //Valida si la letra ingresada es igual a la letra de la palabra
-        if(this.run.palabra[i].valor === letra){
+        if (this.run.palabra[i].valor === letra) {
           this.run.palabra[i].adivinada = true;
           adivinada = true;
         }
         // Si no hay letras por adivinar, el jugador gana
-        if(!this.run.palabra[i].adivinada){
+        if (!this.run.palabra[i].adivinada) {
           esGanador = false;
         }
       }
 
-      if(adivinada){
+      if (adivinada) {
         // Marca el elemento como adivinado       
-        for(let i = 0; i < this.letraElements.length; i++){
-          if(this.letraElements.toArray()[i].nativeElement.innerText === letra){
+        for (let i = 0; i < this.letraElements.length; i++) {
+          if (this.letraElements.toArray()[i].nativeElement.innerText === letra) {
             this.letraElements.toArray()[i].nativeElement.classList.add('adivinada');
             break
           }
         }
 
-        if(esGanador){
-          this.gameState = this.gameStateList[2]; // win
-          this.screenEnd.nativeElement.classList.remove('hidden');
+        if (esGanador) {
+          this.gameState = this.gameStateList[2]; // end
+          this.run.result = 'win';
           console.log("Ganaste");
         }
       }
-      else{
-        for(let i = 0; i < this.letraElements.length; i++){
-          if(this.letraElements.toArray()[i].nativeElement.innerText === letra){
+      else {
+        for (let i = 0; i < this.letraElements.length; i++) {
+          if (this.letraElements.toArray()[i].nativeElement.innerText === letra) {
             this.letraElements.toArray()[i].nativeElement.classList.add('equivocada');
             break
           }
@@ -93,24 +94,24 @@ export class AhorcadoComponent {
         console.log("Perdiste 1 vida: " + this.run.vidas);
         this.MostrarParte();
 
-        if(this.run.vidas === 0){
-          this.gameState = this.gameStateList[3]; // lose
-          this.screenEnd.nativeElement.classList.remove('hidden');
+        if (this.run.vidas === 0) {
+          this.gameState = this.gameStateList[2]; // end
+          this.run.result = 'lose';
           console.log("Fin del juego");
         }
       }
     }
-    else{
+    else {
       console.log("No te quedan vidas");
     }
   }
 
-  ElegirPalabra(dificultad: string){
+  ElegirPalabra(dificultad: string) {
     document.getElementsByClassName('screen-dificult').item(0)!.classList.add('hidden');
     let palabra_array = [];
     let palabraElegida: string = "DEFAULT";
-    
-    switch(dificultad){
+
+    switch (dificultad) {
       case 'facil':
         palabraElegida = this.palabrasFacil[Math.floor(Math.random() * this.palabrasFacil.length)];
         break;
@@ -128,19 +129,26 @@ export class AhorcadoComponent {
         valor: palabra_array[i],
         adivinada: false
       }
-      if(i === 0 || i === palabra_array.length - 1){
+      if (i === 0 || i === palabra_array.length - 1) {
         letra.adivinada = true;
       }
       this.run.palabra.push(letra);
+      this.gameState = this.gameStateList[1]; // playing
     }
     console.log(this.run.palabra)
   }
 
-  Restart(){
+  Continuar() {
+    this.gameState = this.gameStateList[4]; // dificulty
+    this.run.letrasUsadas = [];
+    this.run.palabra = [];
+  }
+  Restart() {
     this.gameState = this.gameStateList[0]; // start
     this.run.vidas = 6;
     this.run.palabra = [];
     this.run.letras = [];
+    this.run.result = '';
     this.screenEnd.nativeElement.classList.add('hidden');
     this.letraElements.forEach((letra) => {
       letra.nativeElement.classList.remove('adivinada');
@@ -151,8 +159,8 @@ export class AhorcadoComponent {
     this.run.letrasUsadas = [];
   }
 
-  MostrarParte(){
-    switch(this.run.vidas){
+  MostrarParte() {
+    switch (this.run.vidas) {
       case 5:
         this.run.partesPersonaje.cabeza.visible = true;
         this.run.partesPersonaje.cabeza.elemento.nativeElement.classList.add('visible');
@@ -174,12 +182,12 @@ export class AhorcadoComponent {
         this.run.partesPersonaje.cuerpo.visible = true;
         this.run.partesPersonaje.cuerpo.elemento.nativeElement.classList.add('visible');
         break;
-      
+
       case 1:
         this.run.partesPersonaje.piernaIzq.visible = true;
         this.run.partesPersonaje.piernaIzq.elemento.nativeElement.classList.add('visible');
         break;
-      
+
       case 0:
         this.run.partesPersonaje.piernaDer.visible = true;
         this.run.partesPersonaje.piernaDer.elemento.nativeElement.classList.add('visible');
@@ -187,8 +195,8 @@ export class AhorcadoComponent {
     }
   }
 
-  InitCharacter(){
-    if(this.partElements.length === 0){
+  InitCharacter() {
+    if (this.partElements.length === 0) {
       setTimeout(() => {
         this.InitCharacter();
       }, 100);
@@ -204,5 +212,9 @@ export class AhorcadoComponent {
       piernaIzq: { elemento: this.partElements.get(5)!, visible: false },
       piernaDer: { elemento: this.partElements.get(6)!, visible: false }
     };
+  }
+
+  VerPuntuacion() {
+    this.gameState = this.gameStateList[3]; // score
   }
 }
